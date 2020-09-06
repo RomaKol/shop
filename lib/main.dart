772dec609ae1f701cb1c2546077bc483c402043b
Kolesnikov Roman <kolesnikov.roman.vl@gmail.com>
2020-09-06 09:41:50 +0300

    add user products screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,6 +46,8 @@ class MyApp extends StatelessWidget {
               ProductDetailScreen(),
           CartScreen.routeName: (BuildContext context) => CartScreen(),
           OrdersScreen.routeName: (BuildContext context) => OrdersScreen(),
+          UserProductsScreen.routeName: (BuildContext context) =>
+              UserProductsScreen(),
         },
       ),
     );
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your product'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) => Column(
+            children: <Widget>[
+              UserProductItem(
+                productData.items[index].title,
+                productData.items[index].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productData.items.length,
+        ),
+      ),
+      drawer: AppDrawer(),
+    );
+  }
+}
