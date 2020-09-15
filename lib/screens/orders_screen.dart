28b921ac062413883b14851f440ba035a5a51e29
Kolesnikov Roman <kolesnikov.roman.vl@gmail.com>
2020-09-15 23:31:19 +0300

    add FutureBuilder for orders screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -17,33 +17,56 @@ class _OrdersScreenState extends State<OrdersScreen> {
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((response) async {
-      setState(() {
-        this._isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchSetOrders();
-      setState(() {
-        this._isLoading = false;
-      });
-    });
+    // Future.delayed(Duration.zero).then((response) async {
+    //   setState(() {
+    //     this._isLoading = true;
+    //   });
+    //   await Provider.of<Orders>(context, listen: false).fetchSetOrders();
+    //   setState(() {
+    //     this._isLoading = false;
+    //   });
+    // });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders!'),
       ),
-      body: this._isLoading
-          ? CircularProgressIndicator()
-          : ListView.builder(
-              itemBuilder: (BuildContext context, int index) =>
-                  OrderItem(orderData.orders[index]),
-              itemCount: orderData.orders.length,
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              //  Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (BuildContext context, int index) =>
+                      OrderItem(orderData.orders[index]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
+      // this._isLoading
+      //   ? Center(child: CircularProgressIndicator())
+      //   : ListView.builder(
+      //       itemBuilder: (BuildContext context, int index) =>
+      //           OrderItem(orderData.orders[index]),
+      //       itemCount: orderData.orders.length,
+      //     ),
       drawer: AppDrawer(),
     );
   }
